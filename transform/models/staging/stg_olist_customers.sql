15ac1a4d4a57b540408f9aca7fed78046526b8dd
Okezie Ben John <benjaminokezie.ob@gmail.com>
2026-06-21 20:34:11 +0100

    Refactor SQL for customer data transformation

--- transform/models/staging/stg_olist_customers.sql
+++ transform/models/staging/stg_olist_customers.sql
@@ -1,14 +1,18 @@
 with source as (
-        select * from {{ source('olist', 'customers') }}
-  ),
-  renamed as (
-      select
-          cast(customer_id as varchar)  as customer_id,
-          cast(customer_unique_id as varchar) as customer_unique_id,
-          cast(customer_zip_code_prefix as int) as customer_zip_code_prefix,
-          cast(customer_city as varchar) as customer_city,
-          cast(customer_state as varchar) as customer_state
+        select * from {{ source('olist_raw_dataset', 'olist_customer') }}
+),
 
-      from source
-  )
-  select * from renamed
+
+renamed as (
+    select
+        cast(customer_id as string)  as customer_id,
+        cast(customer_unique_id as string) as customer_unique_id,
+        cast(customer_zip_code_prefix as integer) as customer_zip_code_prefix,
+        cast(customer_city as string) as customer_city,
+        cast(customer_state as string) as customer_state
+
+    from source
+)
+
+
+select * from renamed
