b5f6edf0f80ca5a8e7231d8c9cb25f27428cf169
Okezie Ben John <benjaminokezie.ob@gmail.com>
2026-06-22 05:01:29 +0100

    Change source dataset and update column types

--- transform/models/staging/stg_olist_order_payments.sql
+++ transform/models/staging/stg_olist_order_payments.sql
@@ -1,12 +1,12 @@
 with source as (
-        select * from {{ source('olist', 'order_payments') }}
+        select * from {{ source('olist_raw_dataset', 'order_payments') }}
   ),
   renamed as (
       select
-          cast(order_id as varchar) as order_id,
+          cast(order_id as string) as order_id,
           cast(payment_sequential as integer) as payment_sequential,
-          cast(payment_type as varchar) as payment_type,
-          cast(payment_value as float) as payment_value,
+          cast(payment_type as string) as payment_type,
+          cast(payment_value as float64) as payment_value,
           cast(payment_installments as integer) as payment_installments
       from source
   )
